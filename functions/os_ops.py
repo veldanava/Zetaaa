3e73680b77ef3236e7036489eba42d8d61828127
zetaaa <86060881+veldanava@users.noreply.github.com>
2023-01-30 22:00:57 +0700

    Update os_ops.py

--- functions/os_ops.py
+++ functions/os_ops.py
@@ -5,7 +5,7 @@ import subprocess as sp
 # path 
 paths = {
     'notepad': "C:\\Program Files\\Notepad++\\notepad++.exe",
-    'discord': "C:\\Users\\ashut\\AppData\\Local\\Discord\\app-1.0.9003\\Discord.exe",
+    'discord': "C:\\Users\\equinox\\AppData\\Local\\Discord\\app-1.0.9003\\Discord.exe",
     'calculator': "C:\\Windows\\System32\\calc.exe"
 }
 
